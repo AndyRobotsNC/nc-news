559cd0335b45e76505b54cd00773fa0e2d5b0ebe
andy <andyrobotsdroid@gmail.com>
2022-01-13 16:32:06 +0000

    all sort_by working

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -8,11 +8,14 @@ const SortBy = ({ setSort_By }) => {
       <form>
         <label for="sort_by">Sort by:</label>
         <select onChange={handleChange} id="list" name="sort_by" id="sort_by">
-          <option disabled selected value="-------">
-            -------
+          <option disabled selected value="please_select">
+            Please select
           </option>
+
           <option value="created_at">date</option>
+
           <option value="votes">votes</option>
+          <option value="comment_count">comments</option>
         </select>
       </form>
     </div>
